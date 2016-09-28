24e690bbd81ea94e67297d172b79f83da41169f7
chenpeixin <chenpeixin@renrenche.com>
2016-09-28 18:49:39 +0800

    将AliyunMNS的PHP SDK转为composer形式

---
+++ src/Exception/InvalidArgumentException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace AliyunMNS\Exception;
+
+use AliyunMNS\Exception\MnsException;
+
+class InvalidArgumentException extends MnsException
+{
+}
---
+++ src/Exception/TopicNotExistException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace AliyunMNS\Exception;
+
+use AliyunMNS\Exception\MnsException;
+
+class TopicNotExistException extends MnsException
+{
+}
